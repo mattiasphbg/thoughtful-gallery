9d6d2198f379ebf077c6ba5d518b835bc9c29f70
mattiasphbg <mattiasp@hotmail.com>
2023-10-03 20:12:02 +0200

    send empty href

--- src/components/headfoot/Header.tsx
+++ src/components/headfoot/Header.tsx
@@ -22,7 +22,7 @@ const Header: React.FC = () => {
           <div className="!visible hidden grow basis-[100%] items-center lg:!flex lg:basis-auto" id="navbarSupportedContentX" data-te-collapse-item>
             <ul className="mr-auto flex flex-col lg:flex-row" data-te-navbar-nav-ref>
               <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-                <a className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90" href="" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">Home</a>
+                <link className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90" href="/" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">Home</link>
               </li>
               <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
                 <link className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90" href="/adventure" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">Adventure</link>
