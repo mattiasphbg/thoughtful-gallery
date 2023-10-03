500c71ba74d6ede09f38d2396144cbb427ab3e4e
mattiasphbg <mattiasp@hotmail.com>
2023-10-03 20:06:03 +0200

    jsx to tsx

--- src/components/headfoot/Header.tsx
+++ src/components/headfoot/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Header = () => {
+const Header: React.FC = () => {
   return (
     <header>
       {/* Navigation bar */}
@@ -25,16 +25,16 @@ const Header = () => {
                 <a className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90" href="" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">Home</a>
               </li>
               <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-                <a className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90" href="/adventure" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">Adventure</a>
+                <link className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90" href="/adventure" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">Adventure</link>
               </li>
               <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-                <a className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90" href="/feedback" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">Feedback</a>
+                <link className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90" href="/feedback" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">Feedback</link>
               </li>
               <li className="mb-2 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-                <a className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90" href="/about" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">About</a>
+                <link className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90" href="/about" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">About</link>
               </li>
               <li className="mb-2 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-                <a className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90" href="/news" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">News</a>
+                <link className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90" href="/news" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">News</link>
               </li>
             </ul>
           </div>
