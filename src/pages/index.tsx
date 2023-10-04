e3576741566d9f9da103e7c5ae62a73c146b9622
mattiasphbg <mattiasp@hotmail.com>
2023-10-04 06:07:01 +0200

    removeMada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 import NestedLayout from '../components/nested-layout'
 import type { NextPageWithLayout } from './_app'
 
-
+  
  
 
 // import { api } from "~/utils/api";
@@ -14,9 +14,7 @@ const Page: NextPageWithLayout = () => {
   return (
     <>
       <div className="flex justify-center items-center h-screen">
-    
-    <p className="text-xl text-center">To my dear Magda i love you ❤️:  </p>
-    <img src="https://res.cloudinary.com/dxhfq1g84/image/upload/v1696361128/thumbnail_Screenshot_20230928_131609_WhatsApp_zsmszl.jpg" className='w-52 h-52' alt="" />
+  
   </div>
     </>
   )
