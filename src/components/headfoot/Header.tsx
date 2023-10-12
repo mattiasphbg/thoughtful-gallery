11b0b6bbad085790c25038b0eeb64732e774db15
mattiasphbg <mattiasp@hotmail.com>
2023-10-12 17:45:32 +0200

    Logo

--- src/components/headfoot/Header.tsx
+++ src/components/headfoot/Header.tsx
@@ -1,20 +1,41 @@
 import Link from "next/link";
 import { ModeToggle } from "../ui/darkmodeToggle";
+import { api } from "~/utils/api";
+import { Layers } from "@prisma/client";
+
+type urlType = {
+  id: number;
+  route: string;
+  layer: Layers | null;
+  title: string;
+};
 
 const Header: React.FC = () => {
+  const { data: d } = api.url.getAll.useQuery<urlType[]>();
+
   return (
     <>
       <header className="flex items-center justify-center">
         {/* Navigation bar */}
+
         <nav
           className="relative flex w-full items-center justify-between bg-white py-2 text-neutral-600 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 dark:text-neutral-200 md:flex-wrap md:justify-center"
           data-te-navbar-ref
         >
           <div className="flex w-full flex-wrap items-center justify-center px-3 text-center">
-            <div className="flex items-center">
-              {/* Hamburger menu button */}
-              {/* Rest of the code */}
-            </div>
+            <Link
+              href="/"
+              className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+              data-te-nav-link-ref
+              data-te-ripple-init
+              data-te-ripple-color="light"
+            >
+              <img
+                src="https://cdn1.iconfinder.com/data/icons/logos-and-brands-3/512/84_Dev_logo_logos-512.png"
+                alt="Logo"
+                className=" h-20 w-20"
+              />
+            </Link>
 
             {/* Navigation links */}
             <div
@@ -26,61 +47,23 @@ const Header: React.FC = () => {
                 className="mr-auto flex flex-col justify-center lg:flex-row"
                 data-te-navbar-nav-ref
               >
-                <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-                  <Link
-                    href="/"
-                    className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
-                    data-te-nav-link-ref
-                    data-te-ripple-init
-                    data-te-ripple-color="light"
+                {d?.map((route) => (
+                  <li
+                    className="mb-4 lg:mb-0 lg:pr-2"
+                    data-te-nav-item-ref
+                    key={route.id}
                   >
-                    Home
-                  </Link>
-                </li>
-                <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-                  <Link
-                    href="/adventure"
-                    className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
-                    data-te-nav-link-ref
-                    data-te-ripple-init
-                    data-te-ripple-color="light"
-                  >
-                    Adventure
-                  </Link>
-                </li>
-                <li className="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-                  <Link
-                    href="/feedback"
-                    className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
-                    data-te-nav-link-ref
-                    data-te-ripple-init
-                    data-te-ripple-color="light"
-                  >
-                    Feedback
-                  </Link>
-                </li>
-                <li className="mb-2 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-                  <Link
-                    href="/about"
-                    className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
-                    data-te-nav-link-ref
-                    data-te-ripple-init
-                    data-te-ripple-color="light"
-                  >
-                    About
-                  </Link>
-                </li>
-                <li className="mb-2 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-                  <Link
-                    href="/news"
-                    className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
-                    data-te-nav-link-ref
-                    data-te-ripple-init
-                    data-te-ripple-color="light"
-                  >
-                    News
-                  </Link>
-                </li>
+                    <Link
+                      href={route.route}
+                      className="block transition duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+                      data-te-nav-link-ref
+                      data-te-ripple-init
+                      data-te-ripple-color="light"
+                    >
+                      {route.title}
+                    </Link>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
