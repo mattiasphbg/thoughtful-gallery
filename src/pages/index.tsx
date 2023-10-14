394eea03d777bcda49f7894b2c6925df4c12edff
mattiasphbg <mattiasp@hotmail.com>
2023-10-14 06:43:54 +0200

    t

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,30 +11,33 @@ import { ModeToggle } from "~/components/ui/darkmodeToggle";
 const Page: NextPageWithLayout = () => {
   return (
     <>
-      <div className="relative">
-        <video
-          autoPlay
-          muted
-          loop
-          className="pointer-events-none fixed z-[-1] h-full w-full object-cover"
-        >
-          <source
-            src="https://res.cloudinary.com/dxhfq1g84/video/upload/v1696680216/video/pexels-cottonbro-8388279_1080p_jpov9c.mp4"
-            type="video/mp4"
-          />
-        </video>
-      </div>
+      <section>
+        <div className=" relative">
+          <video
+            autoPlay
+            muted
+            loop
+            className="absolute left-0 top-0 z-[-100] h-auto min-h-full w-auto min-w-full"
+          >
+            <source
+              src="https://res.cloudinary.com/dxhfq1g84/video/upload/v1696680216/video/pexels-cottonbro-8388279_1080p_jpov9c.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
 
-      <div className="w-300 h-100 absolute left-1/2 top-[calc(50%-200px)] flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center rounded-lg">
-        <h1 className="text-center text-5xl capitalize text-white ">
-          Welcome!
-        </h1>
-        <h5 className="line-clamp-1 text-center text-white">
-          Ready for a new adventure?
-        </h5>
-      </div>
+        <div className="absolute left-1/2 top-[calc(50%-200px)] flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center rounded-lg">
+          <h1 className="text-center text-5xl capitalize text-white ">
+            Welcome!
+          </h1>
+          <h5 className="line-clamp-1 text-center text-white">
+            Ready for a new adventure?
+          </h5>
+        </div>
 
-      <ModeToggle />
+        <ModeToggle />
+      </section>
+      <section className=""></section>
     </>
   );
 };
