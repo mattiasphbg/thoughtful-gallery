e4d56932bc5f944be9cd9fa51f28ffc2003c557a
mattiasphbg <mattiasp@hotmail.com>
2023-10-19 19:52:10 +0200

    -konvo reactfiberthree+

--- src/components/ui/twoDBackground.tsx
+++ src/components/ui/twoDBackground.tsx
@@ -1,23 +1,17 @@
 import React from "react";
-React.useLayoutEffect = React.useEffect;
-import { Stage, Layer, Rect, Image } from "react-konva";
-import useImage from "use-image";
-
-const url = "https://konvajs.github.io/assets/yoda.jpg";
+import { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
 function MyTwoCanvas() {
-  const [image] = useImage(url);
-
   return (
     <>
-      <div className="container w-full">
-        <Stage width={window.innerWidth} height={window.innerHeight}>
-          <Layer>
-            <Image x={20} y={20} image={image} draggable alt="Something" />
-            <Rect x={20} y={20} width={100} height={50} fill="red" draggable />
-          </Layer>
-        </Stage>
-      </div>
+      <Canvas>
+        <pointLight position={[10, 10, 10]} />
+        <mesh>
+          <sphereGeometry />
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
+      </Canvas>
     </>
   );
 }
