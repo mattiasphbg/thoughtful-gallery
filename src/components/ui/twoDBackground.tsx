621198e33cd9ad65bd944b95e4ce31ff9d129085
mattiasphbg <mattiasp@hotmail.com>
2023-10-21 17:57:21 +0200

    news page init

--- src/components/ui/twoDBackground.tsx
+++ src/components/ui/twoDBackground.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import * as THREE from 'three';
+
+
 
 function MyTwoCanvas() {
+
+  const img = "https://images.unsplash.com/photo-1515825838458-f2a94b20105a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8Y2l0eSxuaWdodHx8fHx8fDE2OTc4Njc1MjQ&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080";
+  
+  // const colorMap: THREE.Texture = useLoader(, img);
+
   return (
     <>
       <Canvas>
@@ -10,6 +18,7 @@ function MyTwoCanvas() {
         <mesh>
           <sphereGeometry />
           <meshStandardMaterial color="hotpink" />
+          <meshBasicMaterial attach={"material"}/>
         </mesh>
       </Canvas>
     </>
