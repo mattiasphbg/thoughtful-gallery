6b8cc0cd972e5d0353a2cbd104bea2db1769b227
mattiasphbg <mattiasp@hotmail.com>
2023-10-22 13:33:52 +0200

    from windows

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,15 +1,15 @@
 import type { ReactElement } from "react";
-/* import Image from "next/image"; */
+import Image from "next/image";
 import Layout from "../../components/layout";
 import NestedLayout from "../../components/nested-layout";
 import type { NextPageWithLayout } from "../_app";
-/* import { Input } from "~/components/ui/input";
-import { Button } from "~/components/ui/button"; */
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
 
 const Page: NextPageWithLayout = () => {
   return (
     <>
-      {/*  <section className="w-full py-8 md:py-16 lg:py-24">
+      <section className="w-full py-8 md:py-16 lg:py-24">
         <div className="container mx-auto max-w-5xl px-4 md:px-6 lg:px-8">
           <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
             News
@@ -86,7 +86,7 @@ const Page: NextPageWithLayout = () => {
             <Button type="submit">Subscribe</Button>
           </form>
         </div>
-      </section> */}
+      </section>
     </>
   );
 };
