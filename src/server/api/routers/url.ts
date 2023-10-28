03c85cc1606d456ffe10fe580e571f497ae0262c
mattiasphbg <mattiasp@hotmail.com>
2023-10-28 16:59:56 +0200

    Revert "Item datamodel"
    This reverts commit 7b3e8875ae6f2941cce802070d3b61ba6b08de4a.

--- src/server/api/routers/url.ts
+++
@@ -1,9 +0,0 @@
-// import { z } from "zod"; you can use zod for type-saftety
-
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-
-export const urlRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.routes.findMany();
-  }),
-});
