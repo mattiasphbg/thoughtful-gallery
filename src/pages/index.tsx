91dbbb6c2435b8e3a5301b00e7cef3c81aebb2b9
mattiasphbg <mattiasp@hotmail.com>
2023-11-06 16:11:32 +0100

    feat: add image to landingpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,21 +12,31 @@ const Page: NextPageWithLayout = () => {
     return (
         <>
             <main className="flex-1">
-                <section className="w-full bg-zinc-100 py-12 md:py-24 lg:py-32 xl:py-48">
+                <section
+                    className="w-full bg-cover bg-center py-6 sm:py-12 md:py-24 lg:py-32 xl:py-48"
+                    style={{
+                        backgroundImage:
+                            "url(https://res.cloudinary.com/dxhfq1g84/image/upload/v1699282206/thoughtful-gallery/pexels-pixabay-460659_bdv15y.jpg)",
+                    }}
+                >
                     <div className="container px-4 md:px-6">
-                        <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
-                            Welcome to the Virtual Museum
-                        </h1>
-                        <p className="my-4 max-w-[600px] text-zinc-500 dark:text-zinc-400 md:text-xl">
-                            Explore our digital collections and experience
-                            history from the comfort of your home.
-                        </p>
-                        <Link
-                            className="inline-flex h-10 items-center justify-center rounded-md bg-zinc-900 px-8 text-sm font-medium text-zinc-50 shadow transition-colors hover:bg-zinc-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90 dark:focus-visible:ring-zinc-300"
-                            href="#"
-                        >
-                            Begin Tour
-                        </Link>
+                        <div className="flex flex-col justify-center space-y-4">
+                            <div className="space-y-2">
+                                <h1 className="text-3xl font-bold tracking-tighter text-white sm:text-5xl md:text-6xl">
+                                    Welcome to the Virtual Museum
+                                </h1>
+                                <p className="max-w-[600px] text-white md:text-xl">
+                                    Discover the wonders of art and history from
+                                    the comfort of your own home.
+                                </p>
+                            </div>
+                            <Link
+                                className=" inline-flex h-10 w-96 items-center justify-center rounded-md bg-white px-8 text-sm font-medium text-black shadow transition-colors hover:bg-gray-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-black"
+                                href="#"
+                            >
+                                Start Exploring
+                            </Link>
+                        </div>
                     </div>
                 </section>
                 <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
