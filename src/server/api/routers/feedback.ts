c88737d7ba78efa66505c3b931931b2752091489
mattiasphbg <mattiasp@hotmail.com>
2023-11-09 15:33:17 +0100

    fix: add the fields correcly

--- src/server/api/routers/feedback.ts
+++ src/server/api/routers/feedback.ts
@@ -1,42 +1,37 @@
-import { feedback } from '~/server/api/routers/feedback';
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { z } from "zod"
+import { z } from "zod";
+import { db } from "~/server/db";
 
 export const feedback = createTRPCRouter({
     getAll: publicProcedure.query(({ ctx }) => {
         const items = ctx.db.feedback.findMany();
-        // Validate the response using the Zod schema
-        // zDataChecker.parse(items);
-
         return items;
     }),
-    getOneIName: publicProcedure.query(async ({ ctx, input }) => {
-        const item = await ctx.db.feedback.findFirst({
-            where: {
-                name: input,
-            },
-        });
-
-        if (!item) {
-            // Handle the case where the item is not found
-            throw new Error("Item not found");
-        }
-
-        // Validate the response using the Zod schema if needed
-        // itemResponseSchema.parse(item);
-
-        return item;
-    }),
     add: publicProcedure
-    .input(
-      z.object({
-        id: z.string().optional(),
-        text: z.string().min(1),
-      }),
-    )
-    .mutation(async ({ ctx, input }) => {
-      const todo = await ctx.db.feedback.create({
-        data: input,
-      });
-      return todo;
-    }),
+        .input(
+            z.object({
+                id: z.string().uuid().optional(),
+                name: z.string(),
+                email: z.string().email(),
+                message: z.string().min(1),
+            }),
+        )
+        .mutation(async ({ input }) => {
+            const feedback = await db.feedback.create({
+                data: {
+                    id: input.id !== undefined ? parseInt(input.id) : undefined,
+                    name: input.name,
+                    email: input.email,
+                    message: input.message,
+                },
+                select: {
+                    id: true,
+                    name: true,
+                    email: true,
+                    message: true,
+                    createdAt: true,
+                },
+            });
+            return feedback;
+        }),
+});
