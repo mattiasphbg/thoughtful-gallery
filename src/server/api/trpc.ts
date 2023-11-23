507237884af5fd82aeb417e9c97c2084dc5e112f
mattiasphbg <mattiasp@hotmail.com>
2023-11-23 17:50:27 +0100

    feat: user dont exist create one in database

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -65,11 +65,27 @@ export const createTRPCContext = async (opts: CreateNextContextOptions) => {
     let identity: userIdentity | undefined | null = null;
 
     if (userId) {
-        const id = parseInt(userId, 10);
+        console.log("Here", userId);
+        identity = await db.userIdentity.findFirst({
+            where: { clerkId: userId },
+        });
 
-        if (!isNaN(id)) {
-            identity = await db.userIdentity.findUnique({
-                where: { id },
+        if (identity === null) {
+            const defaultFullName = "Default FullName";
+            const defaultEmail = "default@example.com";
+
+            identity = await db.userIdentity.create({
+                data: {
+                    name: defaultFullName, // Replace with actual name from user object
+                    email: defaultEmail, // Replace with actual email from user object
+                    jobTitle: null, // Set jobTitle to null for now
+                    imageUrl: null, // Set imageUrl to null for now
+                    globalAdmin: null, // Set globalAdmin to null for now
+                    clerkId: userId, // Assign the userId obtained from the session
+                    createdAt: new Date(), // Set createdAt to the current date
+                    updatedAt: new Date(), // Set updatedAt to the current date
+                    role: "USER", // Provide a default role or obtain the role from the user object
+                },
             });
         }
     }
