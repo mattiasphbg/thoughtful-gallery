0c0e4f305555286bb3ce725733fb568288273559
mattiasphbg <mattiasp@hotmail.com>
2023-12-08 16:37:37 +0100

    feat: add the scene to  adventure home;

---
+++ src/components/ui/imageWithFrames.tsx
@@ -0,0 +1,47 @@
+import * as THREE from "three";
+import { useEffect, useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+    useCursor,
+    MeshReflectorMaterial,
+    Image,
+    Text,
+    Environment,
+} from "@react-three/drei";
+import { useRoute, useLocation } from "wouter";
+import { easing } from "maath";
+import getUuid from "uuid-by-string";
+
+const GOLDENRATIO = 1.61803398875;
+
+function ImageWithFrames({ images }) {
+    return (
+        <>
+            <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
+                <color attach={"background"} args={["#191920"]} />
+                <fog attach={"fog"} args={["#191920", 0, 15]} />
+                <group position={[0, -0.5, 0]}>
+                    <mesh rotation={[-Math.PI / 2, 0, 0]}>
+                        <planeGeometry args={[50, 50]} />
+                        <MeshReflectorMaterial
+                            blur={[300, 100]}
+                            resolution={2048}
+                            mixBlur={1}
+                            mixStrength={80}
+                            roughness={1}
+                            depthScale={1.2}
+                            minDepthThreshold={0.4}
+                            maxDepthThreshold={1.4}
+                            color="#050505"
+                            metalness={0.5}
+                            mirror={0}
+                        />
+                    </mesh>
+                </group>
+                <Environment preset={"city"} />
+            </Canvas>
+        </>
+    );
+}
+
+export default ImageWithFrames;
--- src/pages/adventure/index.tsx
+++ src/pages/adventure/index.tsx
@@ -2,27 +2,23 @@ import type { ReactElement } from "react";
 import Layout from "../../components/layout";
 import NestedLayout from "../../components/nested-layout";
 import type { NextPageWithLayout } from "../_app";
-import MyTwoCanvas from "~/components/ui/twoDBackground";
+import ImageWithFrames from "~/components/ui/imageWithFrames";
 const Page: NextPageWithLayout = () => {
-  return (
-    <>
-      <section className=" h-screen">
-        <MyTwoCanvas />
-      </section>
-
-      <section className="h-screen">
-        <div>Content of the third section</div>
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section className=" h-screen">
+                <ImageWithFrames />
+            </section>
+        </>
+    );
 };
 
 Page.getLayout = function getLayout(page: ReactElement) {
-  return (
-    <Layout>
-      <NestedLayout>{page}</NestedLayout>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <NestedLayout>{page}</NestedLayout>
+        </Layout>
+    );
 };
 
 export default Page;
