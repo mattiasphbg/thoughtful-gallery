15df162f5abe84d806a6a684eee631406e4e3d50
mattiasphbg <mattiasp@hotmail.com>
2023-12-10 15:31:03 +0100

    feat: adventure index photo

--- src/pages/adventure/index.tsx
+++ src/pages/adventure/index.tsx
@@ -3,11 +3,54 @@ import Layout from "../../components/layout";
 import NestedLayout from "../../components/nested-layout";
 import type { NextPageWithLayout } from "../_app";
 import ImageWithFrames from "~/components/ui/imageWithFrames";
+
+const pexel = (id: number) =>
+    `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`;
+const images = [
+    // Front
+    { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970) },
+    // Back
+    { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430) },
+    // { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452) },
+    // // Left
+    // {
+    //     position: [-1.75, 0, 0.25],
+    //     rotation: [0, Math.PI / 2.5, 0],
+    //     url: pexel(327482),
+    // },
+    // {
+    //     position: [-2.15, 0, 1.5],
+    //     rotation: [0, Math.PI / 2.5, 0],
+    //     url: pexel(325185),
+    // },
+    // {
+    //     position: [-2, 0, 2.75],
+    //     rotation: [0, Math.PI / 2.5, 0],
+    //     url: pexel(358574),
+    // },
+    // // Right
+    // {
+    //     position: [1.75, 0, 0.25],
+    //     rotation: [0, -Math.PI / 2.5, 0],
+    //     url: pexel(227675),
+    // },
+    // {
+    //     position: [2.15, 0, 1.5],
+    //     rotation: [0, -Math.PI / 2.5, 0],
+    //     url: pexel(911738),
+    // },
+    // {
+    //     position: [2, 0, 2.75],
+    //     rotation: [0, -Math.PI / 2.5, 0],
+    //     url: pexel(1738986),
+    // },
+];
+
 const Page: NextPageWithLayout = () => {
     return (
         <>
             <section className=" h-screen">
-                <ImageWithFrames />
+                <ImageWithFrames images={images} />
             </section>
         </>
     );
